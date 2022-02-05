8cc1da306025bed9a1b0942cc3cb6bce4b3d92d6
Jacob Lamb <jacoblambthomas@gmail.com>
2022-02-05 08:50:45 -0800

    Remove blobity dependency

--- build/_snowpack/pkg/date-holidays.js
+++ build/_snowpack/pkg/date-holidays.js
@@ -1,4 +1,18 @@
-import { a as createCommonjsModule, c as commonjsGlobal } from './common/_commonjsHelpers-7b5f3d4c.js';
+var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+
+function createCommonjsModule(fn, basedir, module) {
+	return module = {
+		path: basedir,
+		exports: {},
+		require: function (path, base) {
+			return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+		}
+	}, fn(module, module.exports), module.exports;
+}
+
+function commonjsRequire () {
+	throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
+}
 
 //! moment.js
 //! version : 2.29.1
