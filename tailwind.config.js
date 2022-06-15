29ec7c71f1fa832fc4ef8e9bf2b49ae435e57115
Jacob Lamb <jacobtlamb@hey.com>
2022-06-14 18:56:12 -0700

    Update website
    - Remove tailwind/typography plugin
    - Update dependencies
    - Refactor css and html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
@@ -9,40 +10,9 @@ module.exports = {
       xl: "2560px",
     },
     extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            "@media (prefers-color-scheme: dark)": {
-              h1: {
-                color: theme("colors.gray.200"),
-              },
-              h2: {
-                color: theme("colors.gray.200"),
-              },
-              a: {
-                color: theme("colors.gray.200"),
-              },
-              '[class~="lead"]': {
-                color: theme("colors.gray.200"),
-              },
-              color: theme("colors.gray.200"),
-            },
-            color: theme("colors.black"),
-            '[class~="lead"]': {
-              color: theme("colors.black"),
-            },
-            maxWidth: "none",
-            a: {
-              textDecoration: "none",
-              color: theme("colors.gray.200"),
-            },
-          },
-        },
-      }),
       fontFamily: {
         sans: ["Lexend Deca", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
 };
