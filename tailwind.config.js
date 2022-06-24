a4513899e939558ede10d8619c12481451d9b097
Jacob Lamb <jacobtlamb@hey.com>
2022-06-23 17:08:39 -0700

    Remove custom theme screen widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      sm: "667px",
-      md: "1024px",
-      lg: "1080px",
-      xl: "2560px",
-    },
     extend: {
       fontFamily: {
         sans: ["Lexend Deca", ...defaultTheme.fontFamily.sans],
