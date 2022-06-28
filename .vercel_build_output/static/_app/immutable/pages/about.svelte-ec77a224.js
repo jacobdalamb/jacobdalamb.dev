0e61051e6a24814b267e53a4fbec5ffbe676233c
Jacob Lamb <jacobtlamb@hey.com>
2022-06-28 15:45:55 -0700

    Build site

---
+++ .vercel_build_output/static/_app/immutable/pages/about.svelte-ec77a224.js
@@ -0,0 +1,11 @@
+import{S as k,i as _,s as S,e as l,t as v,k as x,c,a as m,h as y,d,m as T,b,g as U,F as n,n as f}from"../chunks/index-ed5415b0.js";function E(I){let e,t,a,h,p,o,u;return{c(){e=l("section"),t=l("div"),a=l("h1"),h=v("ABOUT"),p=x(),o=l("p"),u=v(`My passion for web development and design started sometime in 2017 after I had learned that my
+			uncle was a programmer and wanted to learn more of what it's all about. Soon after, I quickly
+			downloaded a code editor and started experimenting with HTML and CSS. Since then, I have
+			increased my knowledge from various web development and UX/UI design courses. Today I continue
+			to level up my skills, my confidence in networking, and building my portfolio. When I'm not
+			working on the web, I love to read books and play games.`),this.h()},l(s){e=c(s,"SECTION",{class:!0});var r=m(e);t=c(r,"DIV",{class:!0});var i=m(t);a=c(i,"H1",{});var g=m(a);h=y(g,"ABOUT"),g.forEach(d),p=T(i),o=c(i,"P",{});var w=m(o);u=y(w,`My passion for web development and design started sometime in 2017 after I had learned that my
+			uncle was a programmer and wanted to learn more of what it's all about. Soon after, I quickly
+			downloaded a code editor and started experimenting with HTML and CSS. Since then, I have
+			increased my knowledge from various web development and UX/UI design courses. Today I continue
+			to level up my skills, my confidence in networking, and building my portfolio. When I'm not
+			working on the web, I love to read books and play games.`),w.forEach(d),i.forEach(d),r.forEach(d),this.h()},h(){b(t,"class","ch-all"),b(e,"class","bg-green-400 dark:bg-green-900")},m(s,r){U(s,e,r),n(e,t),n(t,a),n(a,h),n(t,p),n(t,o),n(o,u)},p:f,i:f,o:f,d(s){s&&d(e)}}}class M extends k{constructor(e){super(),_(this,e,null,E,S,{})}}export{M as default};
