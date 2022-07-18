a2c6657014d63f122598091c21c0ba7aa102d835
Jacob Lamb <jacobtlamb@hey.com>
2022-07-18 12:02:59 -0700

    Change selection color styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,10 @@
 	<div class="ch-all">
 		<h1>Hi, I'm Jacob. <br /> A Frontend Developer</h1>
 		<p class="text-2xl max-w-xl">
-			I'm also a <b class="text-pink-900 dark:text-yellow-200">web designer</b>
+			I'm also a <b
+				class="text-pink-900 dark:text-yellow-200 selection:text-yellow-200 dark:selection:text-pink-900"
+				>web designer</b
+			>
 			who creates high-performant and responsive web apps.
 		</p>
 		<!-- * Best projects showcase (3-6 max) * -->
