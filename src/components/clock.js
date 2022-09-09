039f06babfb1009e7c301306ab6b43da879144f4
Jacob Lamb <jacobtlamb@hey.com>
2022-09-08 19:17:58 -0700

    Update some stuff

--- src/components/clock.js
+++ src/components/clock.js
@@ -3,12 +3,13 @@ import { readable } from 'svelte/store'
 export default function (options = {}) {
   const now = new Date()
 
-  const currentTime = now.getHours() + ':' + now.getMinutes()
+  const currentTime = now.getHours() + ':' + ('0' + now.getMinutes()).slice(-2)
 
   // return a readable store
   return readable(currentTime, (set) => {
     // the `update` function sets the latest date
-    const update = () => set(new Date().getHours() + ':' + new Date().getMinutes())
+    const update = () =>
+      set(new Date().getHours() + ':' + ('0'+ new Date().getMinutes()).slice(-2))
 
     // setup an interval timer to update the store's value every x milliseconds
     const interval = setInterval(update, options.interval || 1000)
