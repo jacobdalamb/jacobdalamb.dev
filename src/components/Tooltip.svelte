81bfa015047cab61b74febfc49df8d68ebdfbbb4
Jacob Lamb <jacobtlamb@hey.com>
2022-09-15 21:34:44 -0700

    Update website
    - update devDependencies
    - add tooltip for footer links

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let title = ''
+  let isHovered = false
+  let x
+
+  function mouseOver(event) {
+    isHovered = true
+    x = event.pageX - 50
+  }
+  function mouseLeave() {
+    isHovered = false
+  }
+</script>
+
+<div class="button" on:mouseover={mouseOver} on:focus={mouseOver} on:mouseleave={mouseLeave}>
+  <slot />
+</div>
+
+{#if isHovered}
+  <div style="top: 765px; left: {x}px;" class="tooltip">{title}</div>
+{/if}
+
+<style>
+  .tooltip {
+    border: 1px solid #ddd;
+    box-shadow: 1px 1px 1px #ddd;
+    background: white;
+    border-radius: 4px;
+    padding: 8px 16px;
+    position: absolute;
+  }
+  @media (prefers-color-scheme: dark) {
+    .tooltip {
+      color: black;
+    }
+  }
+</style>
--- src/components/format.js
+++
@@ -1,4 +0,0 @@
-const formatter = new Intl.RelativeTimeFormat('en')
-const diff = new Date() - new Date('10/11/2022')
-const x = formatter.format(-diff / (1000 * 60 * 60 * 24), 'days')
-console.log(x)
