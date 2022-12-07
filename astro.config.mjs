8762384b3dc19ab327d675c9d8418b3e8809a0ed
Jacob Lamb <jacobtlamb@hey.com>
2022-12-07 00:46:11 -0800

    Revert "Update website"
    This reverts commit f3dcc42c8a51f794e1fd1f5ce279fc7c44a899a8.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,10 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-import purgecss from "astro-purgecss";
 
 export default defineConfig({
 	site: "https://jacoblamb.me",
-	integrations: [
-		svelte(),
-		tailwind({ config: { applyBaseStyles: false } }),
-		purgecss(),
-	],
+	integrations: [svelte(), tailwind()],
 	output: "server",
 	adapter: vercel(),
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,34 +4,35 @@ var chroma = require("chroma-js");
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {
-			fontFamily: {
+        extend: {
+            fontFamily: {
 				sans: ["RubikVariable", ...defaultTheme.fontFamily.sans],
 			},
-		},
+        },
 	},
 	plugins: [
 		function ({ addBase, theme }) {
 			function extractColorVars(colorObj, colorGroup = "") {
-				return Object.keys(colorObj).reduce((vars, colorKey) => {
-					const value = colorObj[colorKey];
+  return Object.keys(colorObj).reduce((vars, colorKey) => {
+    const value = colorObj[colorKey];
 
-					if (typeof value === "object") {
-						// If the value is an object, call extractColorVars recursively on the object
-						// and merge the resulting object into the vars object
-						return { ...vars, ...extractColorVars(value, `-${colorKey}`) };
-					}
+    if (typeof value === "object") {
+      // If the value is an object, call extractColorVars recursively on the object
+      // and merge the resulting object into the vars object
+      return { ...vars, ...extractColorVars(value, `-${colorKey}`) };
+    }
 
-					if (!value.toString().startsWith("#")) {
-						return vars;
-					}
+    if (!value.toString().startsWith("#")) {
+      return vars;
+    }
 
-					const hslValue = chroma(value).css("hsl");
+    const hslValue = chroma(value).css("hsl");
 
-					// Add the new color variable to the vars object
-					return { ...vars, [`--color${colorGroup}-${colorKey}`]: hslValue };
-				}, {});
-			}
+    // Add the new color variable to the vars object
+    return { ...vars, [`--color${colorGroup}-${colorKey}`]: hslValue };
+  }, {});
+}
+
 
 			addBase({
 				":root": extractColorVars(theme("colors")),
