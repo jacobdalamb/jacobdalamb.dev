a2fb9a9ffeaf987eaeedf6ae5cad0b2ace360354
Jacob Lamb <jacobtlamb@hey.com>
2022-12-07 00:55:55 -0800

    Update website
    - Update dependencies
    - Add astro-purgecss
    - Set tailwind config applyBaseStyles to false
    - Put tailwind colors in tw-colors.css and import file in base.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
+import purgecss from "astro-purgecss";
 
 export default defineConfig({
 	site: "https://jacoblamb.me",
+	integrations: [
+		svelte(),
+		tailwind({ config: { applyBaseStyles: false } }),
+		purgecss(),
+	],
 	integrations: [svelte(), tailwind()],
 	output: "server",
 	adapter: vercel(),
