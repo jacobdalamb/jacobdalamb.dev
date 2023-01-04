b45479cc46d341500ea37ab2c659762757f72acc
Jacob Lamb <jacobtlamb@hey.com>
2023-01-04 00:14:53 -0800

    Update website
    - Update dependencies
    - Create Link component
    - Create data for social links

---
+++ src/data/social.js
@@ -0,0 +1,15 @@
+export const social = [
+	{
+		url: "https://read.cv/jacobthesheep",
+		name: "Readme.cv",
+		icon: "fa-memo",
+		style: "fa-light",
+		rotate: "fa-rotate-by",
+	},
+	{
+		url: "https://codepen.io/jacobthesheep",
+		name: "Codepen",
+		icon: "fa-codepen",
+		style: "fa-brands",
+	},
+];
