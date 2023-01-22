975bd3715358495ccd578545dba9b119c648df25
Jacob Lamb <jacobtlamb@hey.com>
2023-01-22 09:58:45 -0800

    Update website
    - Refactor Link component to include prefetch if link is not external and remove arial-label
    - Replace read.cv link icon
    - Update font dependency

--- src/data/social.js
+++ src/data/social.js
@@ -2,9 +2,6 @@ export const social = [
 	{
 		url: "https://read.cv/jacobthesheep",
 		name: "Read.cv",
-		icon: "fa-memo",
-		style: "fa-light",
-		rotate: "fa-rotate-by",
 	},
 	{
 		url: "https://codepen.io/jacobthesheep",
