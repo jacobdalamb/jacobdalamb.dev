be66abac703da70979b3d34705bd2f446327c474
Jacob Lamb <jacobtlamb@hey.com>
2023-02-15 18:08:13 -0800

    Update website
    - Add og home and 404 images
    - Update site url
    - Add PageMeta
    - Restructure head meta data

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-	site: "https://www.jacoblamb.me/",
+	site: "https://www.jtlamb.dev/",
 	output: "server",
 	adapter: vercel(),
 });
