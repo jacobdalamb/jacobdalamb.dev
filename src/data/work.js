50687dde6dbf60f3a2aca0a33900612154685eab
Jacob Lamb <jacobtlamb@hey.com>
2023-04-02 16:50:01 -0700

    Update website design and dependencies

---
+++ src/data/work.js
@@ -0,0 +1,10 @@
+export const work = [
+	{
+		title: "Temple Aviv Judea",
+		site: "https://www.avivjudea.org/",
+	},
+	{
+		title: "Beth Shalom",
+		site: "https://www.bethshalomcorona.com/",
+	},
+];
