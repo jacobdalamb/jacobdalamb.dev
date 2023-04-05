f6bababebff67534a02af17f4701cad09f93fbcb
Jacob Lamb <jacobtlamb@hey.com>
2023-04-05 09:40:23 -0700

    Add postcss-preset-env

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,7 @@ const OpenProps = require("open-props");
 module.exports = {
 	plugins: [
 		require("autoprefixer"),
+		require("postcss-preset-env"),
 		require("postcss-jit-props")({
 			...OpenProps,
 		}),
