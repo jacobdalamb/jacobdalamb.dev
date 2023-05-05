bc3e5c8a78f28a75d1b5d151de1e17bfd9ba6d37
Jacob Lamb <jacobtlamb@hey.com>
2023-05-05 00:47:21 -0700

    Update site
    - Update dependencies
    - Update Link component
    - Other changes...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-import image from "@astrojs/image";
 
 export default defineConfig({
 	site: "https://www.jtlamb.dev/",
 	output: "server",
 	adapter: vercel(),
-	integrations: [image()],
 });
