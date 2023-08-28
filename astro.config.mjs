e324b39c10d65c55b3b8da50b0ba669fc4904012
Jacob Lamb <jacobtlamb@hey.com>
2023-08-28 10:07:09 -0700

    Update site
    - Remove cjs files
    - Add tailwind and another component library
    - Remove some devDependencies
    - Update dependencies
    - Add experimental features from astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://www.jtlamb.dev/",
+  site: "https://portfolio-jacobthesheep.vercel.app/",
+  experimental: {
+    assets: true,
+    viewTransitions: true,
+  },
   output: "server",
-  adapter: vercel({ analytics: true }),
+  adapter: vercel({
+    analytics: true,
+  }),
+  integrations: [react(), tailwind({ applyBaseStyles: false })],
 });
