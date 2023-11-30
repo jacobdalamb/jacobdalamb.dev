e1834c67ad21885b922ec2a8ac5b50a5cd35ab48
Jacob Lamb <jacobtlamb@hey.com>
2023-11-30 14:17:59 -0800

    Remove bookmarks.astro

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -3,9 +3,10 @@ import pkg from "../../package.json";
 const author = pkg.author;
 const parsedAuthor = parse(author);
 const repoURL = pkg.repository.url;
+const description = pkg.description;
 
 const authorName = parsedAuthor.name;
 const authorUrl = parsedAuthor.url;
 const authorUrlString = authorUrl.toString();
 
-export { authorName, authorUrl, repoURL, authorUrlString };
+export { authorName, authorUrl, repoURL, authorUrlString, description };
