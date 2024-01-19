66ddac5a25eb052abc13e0bc16c0d3017d254e82
Jacob Lamb <jacobtlamb@hey.com>
2024-01-18 21:06:50 -0800

    Convert json to plain html and optimize svg api

--- src/utils/getSvgMatch.ts
+++ src/utils/getSvgMatch.ts
@@ -1,41 +1,21 @@
-interface svg {
+interface Svg {
 	id: number;
 	title: string;
 	category: string;
-	route:
-		| string
-		| {
-				dark: string;
-				light: string;
-		  };
 	url: string;
 }
 
-const response = await fetch("https://svgl.app/api/svgs");
-export const svgs = await response.json();
-const svgUrl = "https://svgl.app/library/";
+const SVG_API_URL = "https://svgl.app/api/svgs";
 
-export function getSvgMatch(itemTitle: string, svgs: svg[], svgUrl?: string) {
-	const svgMatch = svgs.find((svg) => {
-		if (svgUrl && itemTitle === "Arc") {
-			return (
-				svg.title === itemTitle && svg.route === `${svgUrl}arc_browser.svg`
-			);
-		}
-		if (svgUrl && itemTitle === "Raycast") {
-			return svg.title === itemTitle && svg.route === `${svgUrl}raycast.svg`;
-		}
-		return svg.title === itemTitle;
-	});
+export const loadSvgs = async (): Promise<Svg[]> => {
+	const response = await fetch(SVG_API_URL);
+	return await response.json();
+};
 
-	let route = "";
-	if (svgMatch?.route) {
-		if (typeof svgMatch.route === "string") {
-			route = svgMatch.route;
-		} else if (typeof svgMatch.route === "object") {
-			route = svgMatch.route.light;
-		}
-	}
-
-	return { svgMatch, route };
-}
+export const getSvgUrl = (
+	itemTitle: string,
+	svgs: Svg[],
+): string | undefined => {
+	const svgMatch = svgs.find((svg) => svg.title === itemTitle);
+	return svgMatch?.url;
+};
