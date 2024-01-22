ed497c396ab6343e309a7fca4e5b4ee03b008eb0
Jacob Lamb <jacobtlamb@hey.com>
2024-01-22 12:04:22 -0800

    Update svg interface

--- src/utils/getSvgMatch.ts
+++ src/utils/getSvgMatch.ts
@@ -1,20 +1,25 @@
-interface Svg {
+export interface svg {
 	id: number;
 	title: string;
 	category: string;
+	route:
+		| string
+		| {
+				dark: string;
+				light: string;
+		  };
 	url: string;
 }
-
 const SVG_API_URL = "https://svgl.app/api/svgs";
 
-export const loadSvgs = async (): Promise<Svg[]> => {
+export const loadSvgs = async (): Promise<svg[]> => {
 	const response = await fetch(SVG_API_URL);
 	return await response.json();
 };
 
 export const getSvgUrl = (
 	itemTitle: string,
-	svgs: Svg[],
+	svgs: svg[],
 ): string | undefined => {
 	const svgMatch = svgs.find((svg) => svg.title === itemTitle);
 	return svgMatch?.url;
